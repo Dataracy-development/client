4db7e0da57050b12a54227ab03156798371d3fb3
Yun Jehyeok <dbswpgur2@naver.com>
2025-10-08 15:14:25 +0900

    edit error

--- src/app/(main)/_components/PopularDatasets.tsx
+++ src/app/(main)/_components/PopularDatasets.tsx
@@ -3,7 +3,7 @@ import { Dataset } from "@/types/commonTypes";
 import { useRouter } from "next/navigation";
 import DatasetRow from "./DatasetRow";
 
-export default function PopularDatasets({ data }: { data: Dataset[] }) {
+export default function PopularDatasets({ data }: { data?: Dataset[] }) {
     const router = useRouter();
     return (
         <div className="bg-n200 py-[80px]">
@@ -11,19 +11,23 @@ export default function PopularDatasets({ data }: { data: Dataset[] }) {
                 <div className="text-h3 mb-2.5 text-center">인기 데이터셋</div>
                 <div className="text-sub1 text-center">데이터러시의 인기 프로젝트를 살펴보고 직접 참여하세요</div>
 
-                <div className="mt-10">
-                    <div className="flex bg-n900 h-[50px] mb-1">
-                        <div className="w-[280px] text-button text-white leading-[50px] text-center">카테고리</div>
-                        <div className="w-[640px] pl-2.5 text-button text-white leading-[50px]">데이터셋</div>
-                        <div className="w-[100px] pl-2.5 text-button text-white leading-[50px]">업데이트일</div>
-                        <div className="w-[200px] text-button text-white leading-[50px] text-center">Icon</div>
+                {data && data.length > 0 ? (
+                    <div className="mt-10">
+                        <div className="flex bg-n900 h-[50px] mb-1">
+                            <div className="w-[280px] text-button text-white leading-[50px] text-center">카테고리</div>
+                            <div className="w-[640px] pl-2.5 text-button text-white leading-[50px]">데이터셋</div>
+                            <div className="w-[100px] pl-2.5 text-button text-white leading-[50px]">업데이트일</div>
+                            <div className="w-[200px] text-button text-white leading-[50px] text-center">Icon</div>
+                        </div>
+                        <div className="flex flex-col gap-1">
+                            {data.map((v) => {
+                                return <DatasetRow key={v.id} data={v} />;
+                            })}
+                        </div>
                     </div>
-                    <div className="flex flex-col gap-1">
-                        {data.map((v) => {
-                            return <DatasetRow key={v.id} data={v} />;
-                        })}
-                    </div>
-                </div>
+                ) : (
+                    <div className="mt-10 text-center text-sub2 text-n600">데이터셋이 없습니다.</div>
+                )}
 
                 <div className="flex justify-center mt-10">
                     <Button label="더 살펴보기" className="w-[140px] h-[60px] rounded-2xl" onClick={() => router.push("/dataset/list")} />
--- src/app/(main)/_components/PopularProjects.tsx
+++ src/app/(main)/_components/PopularProjects.tsx
@@ -12,11 +12,15 @@ export default function PopularProjects({ data }: { data: Project[] }) {
         <div className="max-w-[1200px] w-full mx-auto">
             <div className="text-h3 mb-2.5 text-center">인기 프로젝트</div>
             <div className="text-sub1 text-center">데이터러시의 인기 프로젝트를 살펴보고 직접 참여하세요</div>
-            <div className="grid grid-cols-3 gap-5 gap-y-[21.5px] mt-10">
-                {data.map((v) => {
-                    return <ProjectCard key={v.id} data={v} />;
-                })}
-            </div>
+            {data && data.length > 0 ? (
+                <div className="grid grid-cols-3 gap-5 gap-y-[21.5px] mt-10">
+                    {data.map((v) => {
+                        return <ProjectCard key={v.id} data={v} />;
+                    })}
+                </div>
+            ) : (
+                <div className="mt-10 text-center text-sub2 text-n600">프로젝트가 없습니다.</div>
+            )}
 
             <div className="flex justify-center mt-10">
                 <Button label="더 살펴보기" className="w-[140px] h-[60px] rounded-2xl" onClick={() => router.push("/project/list")} />
